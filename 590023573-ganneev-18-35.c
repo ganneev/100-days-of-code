9585df43f67590b499839b76cec6469a9a5b4b5f
ganneev <ganeevkaur2007@gmail.com>
2025-11-18 09:39:38 +0530

    Add files via upload

---
+++ 590023573-ganneev-18-35.c
@@ -0,0 +1,16 @@
+/*Q35: Write a program to print all factors of a given number.*/
+
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    printf("Factors of %d are: ", n);
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    printf("\n");
+    return 0;
+}
