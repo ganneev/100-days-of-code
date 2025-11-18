85f80ca97075b24a61932376fa5110931c0ff452
ganneev <ganeevkaur2007@gmail.com>
2025-11-18 09:14:42 +0530

    Add files via upload

---
+++ 590023573-ganneev-15-29.c
@@ -0,0 +1,14 @@
+/*Q29: Write a program to calculate the factorial of a number.*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    int factorial = 1;
+    printf("Please enter a positive integer: ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        factorial *= i; 
+    }
+    printf("The factorial of %d is: %d\n", n, factorial);
+    return 0;
+}
